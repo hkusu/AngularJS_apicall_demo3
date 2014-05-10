8e44905919325e65032e7870efac87b7718d53ed
Hiroyuki Kusu <kusu0806@gmail.com>
2014-05-11 01:31:20 +0900

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('apiTestApp', [
+  'ngCookies',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ scripts/services/data.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('apiTestApp')
+  .factory('JsonData', function ($http) {
+
+    return {
+
+      getSampleData: function () {
+
+        return $http.get('data/sample.json')
+          .success(function(data, status, headers, config) {
+
+            // 擬似的に、1秒間ウェイト
+            var time = new Date().getTime();
+            while (new Date().getTime() < time + 15000);
+
+            return data;
+          });
+      }
+
+    }
+
+  })
+;
